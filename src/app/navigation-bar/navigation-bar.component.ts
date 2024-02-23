7fd1c46f6e8704ce3b9c4f5e2029ef838d33a57e
cornrowsMatt <mtuson1414@gmail.com>
2024-02-23 14:44:18 +1300

    visual updates applied and logic defined to get selectedLink and change style

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import {
-  navigationLinks,
-  NavigationLink,
-} from '../models/navigation-link.model';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { navigationLinks, NavigationLink } from '../models/navigation-link.model';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss'],
 })
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnInit, OnDestroy {
+  private selectedLink$ = new BehaviorSubject('home');
+  private destroy$ = new Subject<boolean>();
+
+  selectedLink = '';
   navigationLinks: NavigationLink[] = navigationLinks;
+
+  ngOnInit(): void {
+    this.observeSelectedLink();
+  }
+
+  observeSelectedLink() {
+    this.selectedLink$.pipe(takeUntil(this.destroy$)).subscribe((link) => {
+      this.selectedLink = link;
+      this.navigationLinks.forEach((navLink) => {
+        navLink.selected = navLink.route === link;
+      });
+    });
+  }
+
+  updateSelectedLink(link: NavigationLink) {
+    this.selectedLink$.next(link.route);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }

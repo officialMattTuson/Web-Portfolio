1550175bc9626dd5afb4b5a00c56cf2d989e94c9
cornrowsMatt <mtuson1414@gmail.com>
2024-02-24 16:03:42 +1300

    routing module update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  // { path: 'skills', component: LocationsComponent },
-  // { path: 'education', component: LodgePackageComponent },
-  // { path: 'experience', component: ServicesComponent },
-  // { path: 'contact', component: ServicesComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
